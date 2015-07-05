5ebfe1fd9b9de14cbd24f8a43d1310254bfaf3db
Bryan Miller <gratiafide@yahoo.com>
2015-07-04 22:44:34 -0500

    First commit

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 function curl_get($url)
 {
     return file_get_contents($url);
@@ -12,8 +14,37 @@ $string = curl_get("http://www.extension.harvard.edu/academics/courses/course-ca
 //why it shows up as marked in the browser is because the browser is just interpreting the tags,
 //but the tags can still be parsed just within the php code.  For instance:
 $string = curl_get("http://www.extension.harvard.edu/academics/courses/course-catalog/");
+$myfile = fopen("testfile.txt", "w");
+//fwrite($myfile, $string);
+//echo ($string);
+// CSCI E-10, Intro to Java, Henry Leitner, & CRN#
+/*$four_items_within_each_index = array();
+$four_items_within_each_index = preg_split('/</', strip_tags($string));
+for($k = 0; $k < sizeof($four_items_within_each_index[$k]); $k++) {
+    echo $four_items_within_each_index[$k] . " and k is: " . $k . "<br>";
+    fwrite($myfile, $four_items_within_each_index[$k] . " and k is: " . $k . "<br>");
+}
+*/
 
 //preg_split uses a REGULAR EXPRESSION to split a string.  Gives me more flexibility and power!
+
+$pieces = preg_split('/<td/', $string);
+$newstring = array();
+$j = 0;
+for($i = 0; $i < sizeof($pieces); $i++){
+        $newstring[$j] = "This is array index: " . $i . " and here is the result: " . $pieces[$i];
+        $j++;
+}
+
+for($k = 0; $k < sizeof($newstring); $k++) {
+    fwrite($myfile, $newstring[$k]);
+    echo $newstring[$k] . "<br>";
+}
+
+
+
+//preg_split uses a REGULAR EXPRESSION to split a string.  Gives me more flexibility and power!
+/*
 $pieces = preg_split('/(?=[A-Z])/', strip_tags($string));
 $newstring = array();
 $j = 0;
@@ -36,3 +67,5 @@ for($i = 0; $i < sizeof($pieces); $i++){
 for($k = 0; $k < sizeof($newstring); $k++) {
     echo $newstring[$k] . "<br>";
 }
+
+*/
