507832029bee77e7009d8b7253c25473f117d6b0
Bryan Miller <gratiafide@yahoo.com>
2015-07-04 20:28:14 -0500

    First commit

--- index.php
+++ index.php
@@ -1,46 +1,38 @@
 <?php
 
-/**
- * Application Gateway
- *
- * @author Mark O'Sullivan <mark@vanillaforums.com>
- * @author Todd Burry <todd@vanillaforums.com>
- * @author Tim Gunter <tim@vanillaforums.com>
- * @copyright 2003 Vanilla Forums, Inc
- * @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
- * @package Garden
- * @since 2.0
- */
+function curl_get($url)
+{
+    return file_get_contents($url);
+}
 
-define('APPLICATION', 'Vanilla');
-define('APPLICATION_VERSION', '2.1.11');
+$string = curl_get("http://www.extension.harvard.edu/academics/courses/course-catalog/");
+//echo(strip_tags($string));
 
-// Report and track all errors.
+//If you don't use the strip_tags function, you can still parse the $string - the only reason
+//why it shows up as marked in the browser is because the browser is just interpreting the tags,
+//but the tags can still be parsed just within the php code.  For instance:
+$string = curl_get("http://www.extension.harvard.edu/academics/courses/course-catalog/");
 
-error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
-ini_set('display_errors', 0);
-ini_set('track_errors', 1);
+//preg_split uses a REGULAR EXPRESSION to split a string.  Gives me more flexibility and power!
+$pieces = preg_split('/(?=[A-Z])/', strip_tags($string));
+$newstring = array();
+$j = 0;
+for($i = 0; $i < sizeof($pieces); $i++){
+    if($pieces[$i] == "Jameel") {
+        $j = $j - 1;
+        $newstring[$j] = $pieces[$i]  . " " . $pieces[$i - 2] . " " . $pieces[$i - 1];
+        $i++;
+    } else if($pieces[$i] == "Yvette"){
+        $j = $j - 1;
+        $newstring[$j] = $pieces[$i]  . " " . $pieces[$i - 2] . " " . $pieces[$i - 1];
+        $i++;
+    } else {
+        $newstring[$j] = $pieces[$i + 1] . " " . $pieces[$i];
+    }
+    $i++;
+    $j++;
+}
 
-ob_start();
-
-// Define the constants we need to get going.
-
-define('DS', '/');
-define('PATH_ROOT', getcwd());
-
-// Include the bootstrap to configure the framework.
-
-require_once(PATH_ROOT.'/bootstrap.php');
-
-// Create and configure the dispatcher.
-
-$Dispatcher = Gdn::Dispatcher();
-
-$EnabledApplications = Gdn::ApplicationManager()->EnabledApplicationFolders();
-$Dispatcher->EnabledApplicationFolders($EnabledApplications);
-$Dispatcher->PassProperty('EnabledApplications', $EnabledApplications);
-
-// Process the request.
-
-$Dispatcher->Start();
-$Dispatcher->Dispatch();
+for($k = 0; $k < sizeof($newstring); $k++) {
+    echo $newstring[$k] . "<br>";
+}
